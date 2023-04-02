b809ed8460134053f6281b846c330230f3cbf7a6
zehraaMA <zahraa@thecodepeople.io>
2023-04-02 03:18:08 +0300

    solve the tasks

--- arthimetic-operators.js
+++ arthimetic-operators.js
@@ -1,20 +1,59 @@
-// Arithmetic Operators
+/*******************************************************************************
+ Arithmetic Operators
+ In these exercises, you will be practicing some basic arithmetic and variable manipulation in JavaScript.
+ You will be using the console to log your results and get familiar with different data types in JavaScript.
+ Have fun! 😃
+// Note: Please make sure to limit the number of decimal places to three for any value that may have more than two decimal places by using the .toFixed() method.
+*******************************************************************************/
 
-// Task 1:
-/* In these exercises, you will be practicing some basic arithmetic and variable manipulation in JavaScript. You will be using the console to log your results and get familiar with different data types in JavaScript. These exercises do not require any prior knowledge of programming, but they will help you to become comfortable with some of the core concepts in JavaScript. Have fun!*/
-//1. Subtract two numbers and log the result to the console.
-//2. Find the product of four consecutive odd numbers and log the result to the console.
-//3. Calculate the volume of a rectangular prism with dimensions 3x5x7 and log the result to the console.
-//4. Declare a variable price and assign it the value 9.99. Declare another variable discount and assign it the value 0.20. Calculate the discounted price and log the result to the console.
-//5. Declare a variable message and assign it a string value. Use string interpolation to add more text to the message and log the result to the console.
-//6. Divide two numbers and round the result to the nearest integer before logging it to the console.
-
-// Task 2:
-/* In this task, you will use JavaScript to perform some basic math operations. Follow the steps below to complete the task.*/
-//1. Define variables a, b, c, d, and e using JavaScript.
+/*******************************************************************************
+ // Task 1:
+    In this task, you will use JavaScript to perform some basic math operations. Follow the steps below to complete the task.
+ *******************************************************************************/
+//1. Define variables a, b, c, d, and e.
+let a, b, c, d, e;
 //2. Set the value of variable a to the result of adding 4 to 6.
+a = 4 + 6;
 //3. Set the value of variable b to the result of multiplying 10 by 5.
+b = 10 * 5;
 //4. Set the value of variable c to the remainder of dividing 17 by 3.
+c = 17 / 3;
 //5. Set the value of variable d to the result of subtracting variable a from variable b.
+d = b - a;
 //6. Set the value of variable e to the sum of variables a, b, c, and d.
-//7. Use console.log() to print the value of variable e to the console.
+e = a + b + c + d;
+//7. Use console.log() to print the value of variable e to the console. (it should result 105.667)
+console.log(e.toFixed(3));
+
+/*******************************************************************************/
+// Task 2:
+
+//1. Subtract two numbers and log the result to the console.
+let subtract = 1 - 3;
+console.log(subtract);
+//2. Multiply together the values of four odd numbers that come one after the other, and then output the final result to the console.
+let firstNum = 1;
+let secondNum = firstNum + 2;
+let thirdNum = secondNum + 2;
+let fourthNum = thirdNum + 2;
+let finalResult = firstNum * secondNum * thirdNum * fourthNum;
+console.log(finalResult);
+
+//3. Calculate the volume of a rectangular prism with dimensions 3x5x7 and log the result to the console.
+let length = 3;
+let width = 5;
+let height = 7;
+let volume = length * width * height;
+console.log(volume);
+
+//4. Declare a variable price and assign it the value 9.99. Declare another variable discount and assign it the value 0.20. Calculate the discounted price and log the result to the console.
+let price = 9.99;
+let discount = 0.2;
+let discountedPrice = price * (1 - discount);
+console.log(discountedPrice.toFixed(3));
+
+//5. Divide two numbers and round the result to the nearest integer before logging it to the console.
+let dividend = 10;
+let divisor = 3;
+let quotient = Math.round(dividend / divisor);
+console.log(quotient);
--- logical-operators.js
+++ logical-operators.js
@@ -1,35 +1,71 @@
 //Logical and Comparison Operators
 
-// TASK 1:
-/* In this task, you will be given a set of expressions and your job is to determine the result of each expression.*/
+/*******************************************************************************
+TASK 1:
+     In this task, you will be presented with a series of expressions that you need to evaluate and determine if the result is true or false.
+ // Have fun! // 😃
+********************************************************************************/
 
-// - const exp1 = 10 >= 10
-// - const exp2 = "dog" == "dog"
-// - const exp3 = true != false
-// - const exp4 = "10" === 10
-// - const exp5= 5 > 4
-// - const exp6= null == undefined
-// - const exp7= "true" == true
-// - const exp8= "false" == false
-// - const exp9= NaN === NaN
-// - const exp10= !false || false
-// - const exp11 = false && !false
-// - const exp12 = "apple" > "pineapple"
-// - const exp13 = "2" > "12"
-// - const exp14 = undefined == null
-// - const exp15 = undefined === nul
+const exp1 = 10 >= 10;
+console.log("🚀 ~ file: logical-operators.js:7 ~ exp1:", exp1);
+const exp2 = "dog" == "dog";
+console.log("🚀 ~ file: logical-operators.js:9 ~ exp2:", exp2);
+const exp3 = true != false;
+console.log("🚀 ~ file: logical-operators.js:11 ~ exp3:", exp3);
+const exp4 = "10" === 10;
+console.log("🚀 ~ file: logical-operators.js:13 ~ exp4:", exp4);
+const exp5 = 5 > 4;
+console.log("🚀 ~ file: logical-operators.js:15 ~ exp5:", exp5);
+const exp6 = null == undefined;
+console.log("🚀 ~ file: logical-operators.js:17 ~ exp6:", exp6);
+const exp7 = "true" == true;
+console.log("🚀 ~ file: logical-operators.js:19 ~ exp7:", exp7);
+const exp8 = "false" == false;
+console.log("🚀 ~ file: logical-operators.js:21 ~ exp8:", exp8);
+const exp9 = NaN === NaN;
+console.log("🚀 ~ file: logical-operators.js:23 ~ exp9:", exp9);
+const exp10 = !false || false;
+console.log("🚀 ~ file: logical-operators.js:25 ~ exp10:", exp10);
+const exp11 = false && !false;
+console.log("🚀 ~ file: logical-operators.js:27 ~ exp11:", exp11);
+const exp12 = "apple" > "pineapple";
+console.log("🚀 ~ file: logical-operators.js:29 ~ exp12:", exp12);
+const exp13 = "2" > "12";
+console.log("🚀 ~ file: logical-operators.js:31 ~ exp13:", exp13);
+const exp14 = undefined == null;
+console.log("🚀 ~ file: logical-operators.js:33 ~ exp14:", exp14);
+const exp15 = undefined === null;
+console.log("🚀 ~ file: logical-operators.js:35 ~ exp15:", exp15);
 
-/************************************************************* */
-// Task 2:
-/* In the following tasks, you will practice using logical operators in JavaScript. */
+/*******************************************************************************
+Task 2:
+    In the following tasks, you will practice using logical operators in JavaScript, in addition to some strings methods.
+// Have fun! // 😃 
+********************************************************************************/
 
-// - Check if a number is between 10 and 20 (inclusive) using the logical AND operator. Log the result to the console.
-// - Check if a number is either less than 5 or greater than 50 using the logical OR operator. Log the result to the console.
-// - Check if a string is either "apple" or "orange" using the logical OR operator. Log the result to the console.
-// - Check if a boolean value is true using the logical NOT operator. Log the result to the console.
-// - Check if a number is even and greater than 10 using the logical AND operator. Log the result to the console.
-// - Check if a number is divisible by both 3 and 5 using the logical AND operator. Log the result to the console.
-// - Check if a string contains the letter "e" using the logical AND operator. Log the result to the console.
-// - Check if a string starts with "Hello" using the logical AND operator. Log the result to the console.
-// - Check if a string ends with "world" using the logical AND operator. Log the result to the console.
-// - Check if a number is either negative or odd using the logical OR operator. Log the result to the console.
+const num = 15;
+const str = "Hakuna Matata";
+const isHappy = false;
+
+// - Check if num is between 10 and 20 (inclusive) using the logical AND operator. Log the result to the console.
+console.log(num > 10 && num < 20);
+// - Check if num is either less than 5 or greater than 50 using the logical OR operator. Log the result to the console.
+console.log(num < 5 || num > 50);
+// - Check if str is either "apple" or "orange" using the logical OR operator. Log the result to the console.
+console.log(str == "apple" || str == "orange");
+// - Check if isHappy value is true using the logical NOT operator. Log the result to the console.
+console.log(!isHappy);
+// - Check if num is even and greater than 10 using the logical AND operator. Log the result to the console.
+console.log(num % 2 == 0 && num > 10);
+// - Check if num is divisible by both 3 and 5 using the logical OR operator. Log the result to the console.
+console.log(num % 3 == 0 || num % 5 == 0);
+// - Check if str contains the letter "e". Log the result to the console.
+console.log(str.includes("e"));
+// - Check if str starts with "Hakuna". Log the result to the console.
+console.log(str.startsWith("Hakuna"));
+// - Check if str ends with "a". Log the result to the console.
+console.log(str.endsWith("a"));
+// - Check if num is either negative or odd using the logical OR operator. Log the result to the console.
+console.log(num < 0 || num % 2 !== 0);
+// - Check if the length of str is greater than num or equal to 40 using logical OR operator. Log the result to the console.
+console.log(str.length > num || str.length == 40);
