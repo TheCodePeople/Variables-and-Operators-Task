c71d124d20ccd34fcb96ba49a286c242bb826b1e
zehraaMA <zahraa@thecodepeople.io>
2023-04-15 21:00:54 +0300

    reorganize the file structure

--- arthimetic-operators.js
+++ arthimetic-operators.js
@@ -1,37 +1,51 @@
 /*******************************************************************************
  Arithmetic Operators
- In these exercises, you will be practicing some basic arithmetic and variable manipulation in JavaScript.
- You will be using the console to log your results and get familiar with different data types in JavaScript.
+   In these exercises, you will be practicing some basic arithmetic and variable manipulation in JavaScript.
+   You will be using the console to log your results and get familiar with different data types in JavaScript.
  Have fun! 😃
-// Note: Please make sure to limit the number of decimal places to three for any value that may have more than two decimal places by using the .toFixed() method.
+ Note: Please make sure to limit the number of decimal places to three for any value that may have more than two decimal places by using the .toFixed() method.
 *******************************************************************************/
 
 /*******************************************************************************
- // Task 1:
-    In this task, you will use JavaScript to perform some basic math operations. Follow the steps below to complete the task.
- *******************************************************************************/
-//1. Define variables a, b, c, d, and e.
+Task 1:
+   In this task, you will use JavaScript to perform some basic math operations. Follow the steps below to complete the task.
+   1. Define variables a, b, c, d, and e.
+   2. Set the value of variable a to the result of adding 4 to 6.
+   3. Set the value of variable b to the result of multiplying 10 by 5.
+   4. Set the value of variable c to the remainder of dividing 17 by 3.
+   5. Set the value of variable d to the result of subtracting variable a from variable b.
+   6. Set the value of variable e to the sum of variables a, b, c, and d.
+   7. Use console.log() to print the value of variable e to the console. (it should result 105.667)
+*******************************************************************************/
+// TODO: ADD YOUR CODE HERE
+//1.
 let a, b, c, d, e;
-//2. Set the value of variable a to the result of adding 4 to 6.
+//2.
 a = 4 + 6;
-//3. Set the value of variable b to the result of multiplying 10 by 5.
+//3.
 b = 10 * 5;
-//4. Set the value of variable c to the remainder of dividing 17 by 3.
+//4.
 c = 17 / 3;
-//5. Set the value of variable d to the result of subtracting variable a from variable b.
+//5.
 d = b - a;
-//6. Set the value of variable e to the sum of variables a, b, c, and d.
+//6.
 e = a + b + c + d;
-//7. Use console.log() to print the value of variable e to the console. (it should result 105.667)
+//7.
 console.log(e.toFixed(3));
 
-/*******************************************************************************/
-// Task 2:
-
-//1. Subtract two numbers and log the result to the console.
+/*******************************************************************************
+ Task 2:
+   1. Subtract two numbers and log the result to the console.   
+   2. Multiply together the values of four odd numbers that come one after the other, and then output the final result to the console.
+   3. Calculate the volume of a rectangular prism with dimensions 3x5x7 and log the result to the console.
+   4. Declare a variable price and assign it the value 9.99. Declare another variable discount and assign it the value 0.20. Calculate the discounted price and log the result to the console.
+   5. Divide two numbers and round the result to the nearest integer before logging it to the console.
+*******************************************************************************/
+// TODO: ADD YOUR CODE HERE
+//1.
 let subtract = 1 - 3;
 console.log(subtract);
-//2. Multiply together the values of four odd numbers that come one after the other, and then output the final result to the console.
+//2.
 let firstNum = 1;
 let secondNum = firstNum + 2;
 let thirdNum = secondNum + 2;
@@ -39,20 +53,20 @@ let fourthNum = thirdNum + 2;
 let finalResult = firstNum * secondNum * thirdNum * fourthNum;
 console.log(finalResult);
 
-//3. Calculate the volume of a rectangular prism with dimensions 3x5x7 and log the result to the console.
+//3.
 let length = 3;
 let width = 5;
 let height = 7;
 let volume = length * width * height;
 console.log(volume);
 
-//4. Declare a variable price and assign it the value 9.99. Declare another variable discount and assign it the value 0.20. Calculate the discounted price and log the result to the console.
+//4.
 let price = 9.99;
 let discount = 0.2;
 let discountedPrice = price * (1 - discount);
 console.log(discountedPrice.toFixed(3));
 
-//5. Divide two numbers and round the result to the nearest integer before logging it to the console.
+//5.
 let dividend = 10;
 let divisor = 3;
 let quotient = Math.round(dividend / divisor);
