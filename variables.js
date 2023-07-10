aeedcb5bde8654146835f0c0c726c16b921bb234
walidemad <walidemad313@gmail.com>
2023-07-10 23:38:25 +0300

    solv the task in variables.js file

--- variables.js
+++ variables.js
@@ -15,6 +15,12 @@
     2. Use console.log() to output the value of each variable.
  ********************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+let personName = "ali";
+let age = 20;
+isHappy = true;
+console.log( personName + " "+ age +" "+ isHappy);
+
+
 
 /*******************************************************************************
  Task 2 (Reassigning variables):
@@ -24,6 +30,8 @@
     2. Use console.log o output the value of 'nickName'
  *******************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+let nickName = personName
+console.log("Nickname:" , nickName)
 
 /*******************************************************************************
  Task 3 (Naming variables):
@@ -33,6 +41,8 @@
     2. Declare a variable that stores the age of a user. What name would you choose for this variable?
 *******************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+MyFavoriteMovieTitle= "The Dark Knight";
+userAge=" 30 years Old";
 
 /*******************************************************************************
 Task 4 (String Concatenation):
@@ -48,3 +58,26 @@ Steps:
         - Print the final message to the console, including the personName in uppercase in this format `Dear personName_VALUE, here's your message: finalMsg_VALUE.`.
 *******************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+
+let msg;
+msg = prompt("Enter a message for the person:");
+
+if (isHappy)  {
+let isHappy = true;
+
+msg += " And btw, why are you happy?";
+}
+if (isHappy) {
+msg += " And btw, why are you happy?";
+} else {
+msg += " And btw, Good for you. There's nothing to be happy about.";
+}
+
+let finalMsg = msg;
+
+console.log(`Dear ${personName.toUpperCase()}, here's your message: ${finalMsg}.`);
+
+
+
+
+
