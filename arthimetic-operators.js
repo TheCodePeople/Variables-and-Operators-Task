98ae4d4a87e6cf7655becc08cba156d8451395af
3lawi <alii.3mmar@gmail.com>
2023-07-11 17:16:18 +0300

    Solve arthimetic-operators task

--- arthimetic-operators.js
+++ arthimetic-operators.js
@@ -15,16 +15,37 @@ Task 1:
    4. Set the value of variable c to the remainder of dividing 17 by 3.
    5. Set the value of variable d to the result of subtracting variable a from variable b.
    6. Set the value of variable e to the sum of variables a, b, c, and d.
-   7. Use console.log() to print the value of variable e to the console. (it should result 105.667)
+   7. Use console.log() to print the value of variable e to the console. (it should result 102)
 *******************************************************************************/
 // TODO: ADD YOUR CODE BELOW
 
+
+let a, b, c, d, e;
+a = 4 + 6;
+b = 10 * 5;
+c = 17 % 3;
+d = b - a;
+e = a + b + c + d;
+console.log(e.toFixed(3));
+
+
+
 /*******************************************************************************
  Task 2:
-   1. Subtract two numbers and log the result to the console.   
+   1. Subtract two numbers and log the result to the console.
    2. Multiply together the values of four odd numbers that come one after the other, and then output the final result to the console.
    3. Calculate the volume of a rectangular prism with dimensions 3x5x7 and log the result to the console.
    4. Declare a variable price and assign it the value 9.99. Declare another variable discount and assign it the value 0.20. Calculate the discounted price and log the result to the console.
    5. Divide two numbers and round the result to the nearest integer before logging it to the console.
 *******************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+
+
+console.log(10 - 5);
+console.log(1 * 3 * 5 * 7);
+console.log(3 * 5 * 7);
+let price = 9.99;
+let discount = 0.20;
+let discountedPrice = price - (price * discount);
+console.log(discountedPrice);
+console.log(Math.round(10 / 3));
